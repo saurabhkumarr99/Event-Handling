ab9ea5f45cf1943a89f1a371079f30f19858f752
SAURABH KUMAR RAI <111127201+saurabhkumarr99@users.noreply.github.com>
2022-09-30 14:13:32 +0530

    Add files via upload

---
+++ 30_EventHandling.js
@@ -0,0 +1,23 @@
+function function1() {
+    document.getElementById("result").innerHTML = "Hello , You Have clicked Once";
+}
+
+function function2() {
+    document.getElementById("result").innerHTML = "Hello , You Have clicked Twice";
+}
+
+function function3() {
+    document.getElementById("box2").innerHTML = "Hello , You are inside box";
+}
+
+function function4() {
+    document.getElementById("box2").innerHTML = "Hello , You are outside box";
+}
+
+function function5() {
+    document.getElementById("box3").innerHTML = "Hello , You are scrolling mouse";
+}
+
+function function6() {
+    document.getElementById("box4").innerHTML = "Hello , You have pressed key button";
+}
